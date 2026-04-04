852e6e1cf713b8fc981e54682158326e522aaa32
Ian Yang <145518122+YLGhostP@users.noreply.github.com>
2026-04-04 19:24:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ function speakCharacterWithZhuyin(char, zhuyin) {
     // 先念字
     const utter1 = new SpeechSynthesisUtterance(char);
     utter1.lang = 'zh-TW';
-    utter1.rate = 0.55;
+    utter1.rate = 0.65;
 
     // 停頓後念注音拆解
     utter1.onend = () => {
